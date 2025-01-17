8bf9902e41ff98a8442ffaa899c18769be75e8d4
briansomogyi <brian.somogyi@emanuel.ro>
2025-01-17 14:09:05 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<script setup>
+import SocialStat from './components/SocialStat.vue';
+
+const twitterJSON = {
+  logo: "twitter",
+  title: "Twitter",
+  price: "$500 000",
+  sensIcon: "up",
+  sensText: "+40% more impressions",
+  trialsText: "5% new trials"
+}
+
+const facbookJSON = {
+  logo: "facebok",
+  title: "Facebok",
+  price: "$500 000",
+  sensIcon: "up",
+  sensText: "+40% more impressions",
+  trialsText: "5% new trials"
+}
+
+const tiktokJSON = {
+  logo: "twitter",
+  title: "Facebok",
+  price: "$500 000",
+  sensIcon: "up",
+  sensText: "+40% more impressions",
+  trialsText: "5% new trials"
+}
+
+</script>
+
+<template>
+  <div class="container mx-auto">
+    <div class="gap-6 grid grid-cols-3">
+      <SocialStat :info="twitterJSON" />
+      <SocialStat :info="facbookJSON" />
+      <SocialStat :info="tiktokJSON" />
+      <SocialStat :info="twitterJSON" />
+      <SocialStat :info="twitterJSON" />
+      <SocialStat :info="twitterJSON" />
+    </div>
+  </div>
+</template>
+
+<style>
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+</style>
---
+++ src/components/SocialStat.vue
@@ -0,0 +1,24 @@
+<script setup>
+import TwitterLogo from "../assets/twitter.webp"
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+const props = defineProps(["info"])
+
+let logo = undefined;
+import { onBeforeMount } from "vue"
+onBeforeMount(() => {
+    if (props.info.logo == "twitter") {
+        logo = TwitterLogo;
+    }
+})
+
+</script>
+
+<template>
+    <div v-if="props.info" class="w-full flex flex-col bg-orange-200 p-2 rounded-md drop-shadow-md">
+        <div class="flex"> <img :src="logo" alt="Twitter" class="h-8"> {{ props.info.title }} </div>
+        <div> {{ props.info.price }} </div>
+        <div> <i class="bi bi-arrow-bar-up"></i> {{ props.info.sensText }}</div>
+        <div> {{ props.info.trialsText }} </div>
+    </div>
+</template>
